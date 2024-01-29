282724ac7348dc972e2a6d33182ab584470dff51
Visal-Balakrishnan <147430201+Visal-Balakrishnan@users.noreply.github.com>
2024-01-29 20:40:47 +0530

    Add files via upload

---
+++ p4/p4.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>RANDOM NUMBERS</title>
+    </head>
+    <body>
+        <h1 align="center">RANDOM NUMBERS</h1>
+            <center>
+                <iframe src="random.php"></iframe>
+                <iframe src="random.php"></iframe>
+            </center>
+    </body>
+</html>
---
+++ p4/random.php
@@ -0,0 +1,6 @@
+<html>
+    <center>
+    <br><br><br>
+    <?php echo (rand(1,10)); ?>
+    </center
+</html>
